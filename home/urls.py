819ffae5ac7127263f863ae50da79cdec57d9959
Angelita Santos <angelitasantos@gmail.com>
2022-12-02 20:50:54 -0300

    create about, contact files

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('sobre/', views.about, name='about'),
+    path('contato/', views.contact, name='contact'),
 ]
--- home/views.py
+++ home/views.py
@@ -4,3 +4,13 @@ from django.http import HttpResponse
 def home(request):
     title = 'Home'
     return render(request, 'index.html', {'title': title})
+
+
+def about(request):
+    title = 'Sobre'
+    return render(request, 'about.html', {'title': title})
+
+
+def contact(request):
+    title = 'Contato'
+    return render(request, 'contact.html', {'title': title})
