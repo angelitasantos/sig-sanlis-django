98099f9aa8c229b13fd94881e2e6ca1da87cf0a9
Angelita Santos <angelitasantos@gmail.com>
2022-12-04 11:25:07 -0300

    update company templates

--- activation/views.py
+++ activation/views.py
@@ -4,6 +4,8 @@ from django.contrib import messages
 from django.contrib.messages import constants
 from django.contrib.auth.decorators import login_required
 from .models import Company
+from django.contrib.auth.models import User
+
 
 sig = 'SIG SANLIS | '
 
@@ -22,41 +24,136 @@ def logout(request):
 ## Database Company
 @login_required(login_url='/auth/login/')
 def companies(request):
-    title = sig + 'Empresas'
+    title = sig + 'Listar Empresas'
+    name_filtrar = request.GET.get('name')
     companies = Company.objects.all()
+    users = User.objects.all()
+
+    if name_filtrar:
+        companies = companies.filter(name__icontains = name_filtrar)
 
     context =   {   'title': title,
+                    'users': users,
                     'companies': companies}
     return render(request, 'company/company_list.html', context)
 
 
 @login_required(login_url='/auth/login/')
 def company_create(request):
-    title = sig + 'Empresa'
+    title = sig + 'Criar Empresa'
     if request.method == "GET":
-        return render(request, 'company/company_create.html', {'title': title})
+        users = User.objects.all()
+        context =   {   'title': title,
+                        'users': users}
+        return render(request, 'company/company_create.html', context)
     elif request.method == "POST":
-        return redirect('/auth/nova_empresa')
+        token_company = request.POST.get('token_company')
+        name = request.POST.get('name').upper()
+        full_name = request.POST.get('full_name').upper()
+
+        start_data = request.POST.get('start_data')
+        cnpj = request.POST.get('cnpj')
+        insc_est = request.POST.get('insc_est')
+        insc_mun = request.POST.get('insc_mun')
+
+        street = request.POST.get('street').upper()
+        number1 = request.POST.get('number')
+        number = 0 if number1 == "" else request.POST.get('number')
+        complement = request.POST.get('complement')
+        district = request.POST.get('district').upper()
+        city = request.POST.get('city').upper()
+        state = request.POST.get('state').upper()
+        zipcode = request.POST.get('zipcode')
+        google_maps_link = request.POST.get('google_maps_link')
+
+        email = request.POST.get('email').lower()
+        phone = request.POST.get('phone')
+        whatsapp_number = request.POST.get('whatsapp_number')
+        whatsapp_link = request.POST.get('whatsapp_link').lower()
+
+        site_link = request.POST.get('site_link').lower()
+        instagram_link = request.POST.get('instagram_link').lower()
+        linkedin_link = request.POST.get('linkedin_link').lower()
+        facebook_link = request.POST.get('facebook_link').lower()
+
+        if (len(name.strip()) == 0) or (len(full_name.strip()) == 0):
+            messages.add_message(request, constants.ERROR, 'Preencha todos os campos!!!')
+            return redirect('/auth/nova_empresa/')
+
+        companies = Company.objects.filter(name=name)
+
+        if companies.exists():
+            messages.add_message(request, constants.ERROR, 'Já existe uma empresa cadastrada com este Nome!!!')
+            return redirect('/auth/nova_empresa/')
+
+        try:
+            company = Company(  token_company=token_company,
+                                name=name,
+                                full_name=full_name,
+
+                                start_data=start_data,
+                                cnpj=cnpj,
+                                insc_est=insc_est,
+                                insc_mun=insc_mun,
+
+                                street=street,
+                                number=number,
+                                complement=complement,
+                                district=district,
+                                city=city,
+                                state=state,
+                                zipcode=zipcode,
+                                google_maps_link=google_maps_link,
+
+                                email=email,
+                                phone=phone,
+                                whatsapp_number=whatsapp_number,
+                                whatsapp_link=whatsapp_link,
+
+                                site_link=site_link,
+                                instagram_link=instagram_link,
+                                linkedin_link=linkedin_link,
+                                facebook_link=facebook_link,
+                                user_company=request.user)
+            company.save()
+
+            messages.add_message(request, constants.SUCCESS, 'Empresa Cadastrada com Sucesso!!!')
+            return redirect('/auth/nova_empresa/')
+        except:
+            messages.add_message(request, constants.ERROR, 'Erro Interno do Sistema!!!')
+            return redirect('/auth/nova_empresa/')
 
 
 @login_required(login_url='/auth/login/')
 def company_view(request, id):
-    title = sig + 'Empresa'
+    title = sig + 'Visualizar Empresa'
+    users = User.objects.all()
     company = get_object_or_404(Company, id=id)
     companies = Company.objects.all()
     return render(request, 'company/company_view.html', {   'title': title,
+                                                            'users': users,
                                                             'company': company,
                                                             'companies': companies})
 
 
 @login_required(login_url='/auth/login/')
 def company_update(request, id):
-    title = sig + 'Empresa'
+    title = sig + 'Alterar Empresa'
+    users = User.objects.all()
     company = get_object_or_404(Company, id=id)
     companies = Company.objects.all()
-    return render(request, 'company/company_update.html', { 'title': title,
-                                                            'company': company,
-                                                            'companies': companies})
+    
+    if request.method == "GET":
+        return render(request, 'company/company_update.html', { 'title': title,
+                                                                'users': users,
+                                                                'company': company,
+                                                                'companies': companies})
+    elif request.method == "POST":
+        messages.add_message(request, constants.ERROR, 'Página em Construção!!!')
+        return render(request, 'company/company_update.html', { 'title': title,
+                                                                'users': users,
+                                                                'company': company,
+                                                                'companies': companies})
 
 
 @login_required(login_url='/auth/login/')
@@ -64,4 +161,4 @@ def company_delete(request, id):
     company = Company.objects.get(id=id)
     company.delete()
     messages.add_message(request, constants.SUCCESS, 'Empresa Excluída com Sucesso!')
-    return redirect('auth/empresas')
+    return redirect('/auth/empresas')
