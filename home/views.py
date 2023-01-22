11e4f279aaecea8d75eb1093ae7bea512af7933d
Angelita Santos <angelitasantos@gmail.com>
2023-01-22 08:06:15 -0300

    refactor home pages templates

--- home/views.py
+++ home/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+sig = 'SIG SANLIS | '
+
 def home(request):
-    title = 'Home'
+    title = sig + 'Home'
     return render(request, 'index.html', {'title': title})
 
 
 def about(request):
-    title = 'Sobre'
+    title = sig + 'Sobre'
     return render(request, 'about.html', {'title': title})
 
 
 def contact(request):
-    title = 'Contato'
+    title = sig + 'Contato'
     return render(request, 'contact.html', {'title': title})
